c1d19cb239a79eed48e65948288e3a77d8de6674
Robert Johnstone <rob@plasticviking.com>
2022-04-13 16:43:52 +0800

    add migration support

---
+++ knexfile.ts
@@ -0,0 +1,23 @@
+module.exports = {
+	development: {
+		client: 'pg',
+		searchPath: ['knex', 'public'],
+		connection: {
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT || 5432,
+			database: process.env.DB_DATABASE || 'whis',
+			user: process.env.DB_USER || 'whis',
+			password: process.env.DB_PASSWORD,
+			multipleStatements: true
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'migration',
+			directory: './data/migrations',
+			schemaName: 'knex'
+		}
+	}
+};
